1dd9b42713c04519cb88dd32f5f4a0b8069afbfe
ikedaShuya <k024g0046@g.neec.ac.jp>
2026-06-06 23:01:37 +0900

    初期設定完了

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,33 @@
 #include <Windows.h>
+#include "KamataEngine.h"
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	// エンジンの初期化
+	Initialize(L"LE3C_04_イケダ_シュウヤ");
+
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	// エンジンの終了処理
+	Finalize();
 	
 	return 0;
 }
