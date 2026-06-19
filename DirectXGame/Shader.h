3a8bf0b05bc88a9e5b81d73402fc0d88b80d062b
ikedaShuya <k024g0046@g.neec.ac.jp>
2026-06-19 22:19:49 +0900

    ShaderModel_6_0以上に対応した。

--- DirectXGame/Shader.h
+++ DirectXGame/Shader.h
@@ -3,13 +3,18 @@
 #include <string>  // wstring,string
 #include <d3d12.h> // ID3DBlob
 
+#include <d3dcompiler.h>  // dxcapi.hが依存している
+#include <dxcapi.h>       // IDxcBlobを利用するため
+
 class Shader {
 public:
 	// シェーダーファイルを読み込み、コンパイル済みデータを生成する
-	void Load(const std::wstring& filePath, const std::string& shaderModel);
+	void Load(const std::wstring& filePath, const std::wstring& shaderModel);
+	void LoadDxc(const std::wstring& filePath, const std::wstring& shaderModel);
 
 	// 生成したコンパイル済みデータを取得する
 	ID3DBlob* GetBlob();
+	IDxcBlob* GetDxcBlob();
 
 	// コンストラクタ
 	Shader();
@@ -17,5 +22,7 @@ public:
 	~Shader();
 
 	private:
-	ID3DBlob* blob_ = nullptr; // コンストラクタで初期化しなくていい
+	ID3DBlob* blob_ = nullptr;    // コンストラクタで初期化しなくていい
+
+	IDxcBlob* dxcBlob_ = nullptr; // コンパイル済みのシェーダーデータ
 };
--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -62,20 +62,20 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// 頂点シェーダーの読み込みとコンパイル
 	Shader vs;
-	vs.Load(L"TestVS.hlsl", "vs_5_0");
-	assert(vs.GetBlob() != nullptr);
+	vs.LoadDxc(L"TestVS.hlsl", L"vs_6_0");
+	assert(vs.GetDxcBlob() != nullptr);
 
 	// ピクセルシェーダーの読み込みとコンパイル
 	Shader ps;
-	ps.Load(L"TestPS.hlsl", "ps_5_0");
-	assert(ps.GetBlob() != nullptr);
+	ps.LoadDxc(L"TestPS.hlsl", L"ps_6_0");
+	assert(ps.GetDxcBlob() != nullptr);
 
 	// PSO(PipelineStateObject)の生成
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStateDesc{};
 	graphicsPipelineStateDesc.pRootSignature = rootSignature;  // RootSignature
 	graphicsPipelineStateDesc.InputLayout = inputLayoutDesc;
-	graphicsPipelineStateDesc.VS = {vs.GetBlob()->GetBufferPointer(), vs.GetBlob()->GetBufferSize()}; // VertexShader
-	graphicsPipelineStateDesc.PS = {ps.GetBlob()->GetBufferPointer(), ps.GetBlob()->GetBufferSize()}; // PixelShader
+	graphicsPipelineStateDesc.VS = {vs.GetDxcBlob()->GetBufferPointer(), vs.GetDxcBlob()->GetBufferSize()}; // VertexShader
+	graphicsPipelineStateDesc.PS = {ps.GetDxcBlob()->GetBufferPointer(), ps.GetDxcBlob()->GetBufferSize()}; // PixelShader
 	graphicsPipelineStateDesc.BlendState = blendDesc;           // BlendState
 	graphicsPipelineStateDesc.RasterizerState = rasterizerDesc; // RasterizerState
 	// 書き込みRTVの情報
